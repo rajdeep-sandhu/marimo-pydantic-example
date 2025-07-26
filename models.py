27b2e82f143f00067700a99aef1487731986dede
rajdeep-sandhu <168480080+rajdeep-sandhu@users.noreply.github.com>
2025-07-26 18:58:03 +0100

    add pydantic model Employee

---
+++ models.py
@@ -0,0 +1,51 @@
+# models.py
+
+from datetime import date
+from enum import Enum
+from typing import Self
+from uuid import UUID, uuid4
+
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    EmailStr,
+    Field,
+    field_validator,
+    model_validator,
+)
+
+
+class Department(Enum):
+    HR = "HR"
+    SALES = "SALES"
+    IT = "IT"
+    ENGINEERING = "ENGINEERING"
+
+
+class Employee(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    employee_id: UUID = Field(default_factory=uuid4, frozen=True)
+    name: str = Field(min_length=1, frozen=True)
+    email: EmailStr = Field(pattern=r".+@example\.com$")
+    date_of_birth: date = Field(alias="birth_date", repr=False, frozen=True)
+    salary: float = Field(alias="compensation", gt=0, repr=False)
+    department: Department
+    elected_benefits: bool
+
+    @field_validator("date_of_birth")
+    @classmethod
+    def check_valid_age(cls, date_of_birth: date) -> date:
+        today = date.today()
+        eighteen_years_ago = date(today.year - 18, today.month, today.day)
+        if date_of_birth > eighteen_years_ago:
+            raise ValueError("Employees must be at least eighteen years old.")
+        return date_of_birth
+
+    @model_validator(mode="after")
+    def check_it_benefits(self) -> Self:
+        if self.department == Department.IT and self.elected_benefits:
+            raise ValueError(
+                "IT employees are contractors and don't qualify for benefits."
+            )
+        return self
